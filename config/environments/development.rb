c1b8dfc70369c90e239578eb748d36c16cfd98f7
nucore <nucore@cider>
2018-08-27 12:23:36 -0400

    no idea

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   Rails.application.routes.default_url_options =
-    config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+    config.action_mailer.default_url_options = { host: "https://cider.uconn.edu", port: 443 }
 
   config.action_mailer.perform_caching = false
 
